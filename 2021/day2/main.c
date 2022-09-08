49629f21f5af4ef67613afbad88c8646a84bf61b
Philip Glover <glover.philip@gmail.com>
2022-09-08 18:55:33 -0400

    Finish day 2 part 2, organize htmls and inputs

--- 2021/day2/main.c
+++ 2021/day2/main.c
@@ -6,10 +6,15 @@ int main() {
 	int data_len;
 	char **data_array = open_and_read_file("Welcome to day 2!", "input", &data_len);
 
-	int x, y;
-	find_x_and_y(data_array, data_len, &x, &y);
+	int x, y, aim;
 
-	printf("Found x and y to be (%d, %d) => %d\n", x, y, x * y);
+	printf("Solving part 1...\n");
+	find_x_and_y(data_array, data_len, &x, &y);
+	printf("Found x and y to be (%d, %d) => x * y = %d\n", x, y, x * y);
+
+	printf("Solving part 2...\n");
+	find_aim_x_and_y(data_array, data_len, &aim, &x, &y);
+	printf("Found aim, x, and y to be (%d, %d, %d) => x * y = %d\n", aim, x, y, x * y);
 
 	free_data_array(data_array, data_len);
 	return 0;
@@ -24,7 +29,7 @@ void find_x_and_y(char **data_array, int len, int *x, int *y) {
 
 	for (int i = 0; i < len; i++) {
 		get_move_and_dist(data_array[i], move, &dist);
-		printf("Went %s with a distance of %d\n", move, dist);
+		// printf("Went %s with a distance of %d\n", move, dist);
 		if (strcmp(move, "forward") == 0) {
 			*x += dist;
 		} else if (strcmp(move, "down") == 0) {
@@ -37,14 +42,37 @@ void find_x_and_y(char **data_array, int len, int *x, int *y) {
 	}
 }
 
-void get_move_and_dist(char *instruction, char *move, int *dist) {
-	int idx = 0;
+void find_aim_x_and_y(char **data_array, int len, int *aim, int *x, int *y) {
+	*aim = 0;
+	*x = 0;
+	*y = 0;
 
+	char *move = (char*)malloc(sizeof(char) * 12);
+	int dist;
+
+	for (int i = 0; i < len; i++) {
+		get_move_and_dist(data_array[i], move, &dist);
+		if (strcmp(move, "forward") == 0) {
+			*x += dist;
+			*y += (*aim) * dist;
+		} else if (strcmp(move, "down") == 0) {
+			*aim += dist;
+		} else if (strcmp(move, "up") == 0) {
+			*aim -= dist;
+		} else {
+			fprintf(stderr, "Oops, found an move we don't know how to deal with [%s] at line [%d}", move, i);
+		}
+	}
+}
+
+void get_move_and_dist(char *instruction, char *move, int *dist) {
+	memset(move, 0, strlen(move));
+
+	int idx = 0;
 	while (instruction[idx] != ' ') {
 		idx++;
 	}
 	strncpy(move, instruction, idx);
-	move[idx] = '\000';
 	 *dist = atoi(instruction + idx + 1);
 }
 
--- 2021/day2/main.h
+++ 2021/day2/main.h
@@ -2,4 +2,5 @@
 
 void free_data_array(char **data_array, int data_len);
 void find_x_and_y(char **data_array, int len, int *x, int *y);
+void find_aim_x_and_y(char **data_array, int len, int *aim, int *x, int *y);
 void get_move_and_dist(char *instruction, char *move, int *dist);
