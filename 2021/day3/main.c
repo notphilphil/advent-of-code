543c7a33b704ac7188a3141f95176a0607690553
Philip Glover <glover.philip@gmail.com>
2022-09-12 14:12:32 -0400

    Start day 3 part 2 (split parts

--- 2021/day3/main.c
+++ 2021/day3/main.c
@@ -11,12 +11,28 @@ int main() {
 	int num_of_bits = strlen(data_array[0]) - 1;
 	int *bit_counts = count_bits(data_array, data_len, num_of_bits);
 
+	solve_part_1(bit_counts, num_of_bits);
+	solve_part_2(data_array, bit_counts);
+	return 0;
+}
+
+void solve_part_1(int *bit_counts, int num_of_bits) {
+	printf("Solving part 1...\n");
+
 	int gamma, epsilon;
 	get_gamma_and_epsilon(bit_counts, num_of_bits, &gamma, &epsilon);
 
 	printf("Found gamma and epsilon to be (%d, %d) => gamma * epsilon = %d\n", gamma, epsilon, gamma * epsilon);
+}
 
-	return 0;
+void solve_part_2(char **data_array, int *bit_counts) {
+	printf("Solving part 2...\n");
+
+	int oxygen_generator_rating, co2_scrubber_rating;
+	
+	find_ratings(data_array, bit_counts, oxygen_generator_rating, co2_scrubber_rating);
+
+	printf("Found oxygen generator rating and CO2 scrubber rating to be (%d, %d) => %d * %d = %d\n", oxygen_generator_rating, co2_scrubber_rating, oxygen_generator_rating, co2_scrubber_rating, oxygen_generator_rating * co2_scrubber_rating);
 }
 
 void get_gamma_and_epsilon(int *bit_counts, int num_of_bits, int *gamma, int *epsilon) {
@@ -32,6 +48,12 @@ void get_gamma_and_epsilon(int *bit_counts, int num_of_bits, int *gamma, int *ep
 		*gamma += ((int)pow(2, i)) * is_one;
 		*epsilon += ((int)pow(2, i)) * !is_one;
 	}
+}
+
+void find_ratings(char **data_array, int *bit_counts, int *oxygen_generator_rating, int *co2_scrubber_rating) {
+	*oxygen_generator_rating = 0;
+	*co2_scrubber_rating = 0;
+
 }
 
 int* count_bits(char **data_array, int data_len, int num_of_bits) {
